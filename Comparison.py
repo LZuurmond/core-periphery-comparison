0ae238fc4cff4d42e397705259b327e0c6c11ff8
Lucas <lucas.zuurmond+github@gmail.com>
2025-12-05 01:27:43 +0100

    begin analyze

--- Comparison.py
+++ Comparison.py
@@ -5,11 +5,13 @@ doe voor elke combinatie van 2 oplossingen:
     3. compute final similarity
 resultaat: kxk matrix
 """
-
+# %% imports
 import numpy as np
 from scipy.optimize import linear_sum_assignment
+import tqdm
 
 
+# %% similarity computation
 def jaccard(group_1: list[int], group_2: list[int]) -> float:
     """
     Computes the jaccard similarities of two groups
@@ -34,13 +36,42 @@ def jaccard_matrix(groups_1: list[list[int]], groups_2: list[list[int]]) -> np.n
 
 def compute_similarity(M: np.ndarray) -> dict:
     row_idx, col_idx = linear_sum_assignment(-M)  # linear sum assignment minimizes, so we invert to maximize similarity
-    values = M[row_idx, col_idx]
+    score = M[row_idx, col_idx].mean()
 
     return {
-        "simlarity": values.mean(),
-        "row_idx": list(row_idx),
-        "col_idx": list(col_idx)
+        "similarity": score,
+        "mapping": dict(zip(col_idx, row_idx))
     }
+
+
+# %% Analysis
+def analyze_ensemble(solutions):
+    """
+    Analyzes a set of solutions
+    :param solutions: list[list[list[int]]] representing the different groups
+    """
+    n_samples = len(solutions)
+    print(f"Analyzing  {n_samples} solutions...")
+
+    # step 1: compute similarity matrix
+    sim_matrix = np.zeros((n_samples, n_samples))
+
+    for idx in tqdm.tqdm(range(n_samples * n_samples), desc="Computing pairwise similarities"):
+        i = idx // n_samples
+        j = idx % n_samples
+        res = compute_similarity(jaccard_matrix(solutions[i], solutions[j]))
+        sim_matrix[i, j] = res["similarity"]
+        sim_matrix[j, i] = res["similarity"]
+
+    # find most central solution
+    mean_similarities = sim_matrix.mean(axis=1)
+    central_idx = np.argmax(mean_similarities)
+    central_solution = solutions[central_idx]
+
+    print("\n--- Structural Stability ---")
+    print(f"Overall average similarity: {sim_matrix.mean():.4f}")
+    print(f"Most central solution (highest avg similarity with others): {central_idx+1} (Avg Sim: {mean_similarities[central_idx]:.4f})")
+
 
 
 if __name__ == '__main__':
@@ -49,4 +80,4 @@ if __name__ == '__main__':
     with open("sample_groups.json", 'rt') as f:
         solutions = json.load(f)
 
-    print(compute_similarity(jaccard_matrix(solutions[0], solutions[1])))
+    analyze_ensemble(solutions)
